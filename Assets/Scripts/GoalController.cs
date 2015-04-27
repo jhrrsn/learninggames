28b7c5a2588c93d911ed9b0ffb8911e6dc7f31b3
Jack Harrison <jack@jhrrsn.com>
2015-04-27 09:23:08 +0100

    Initial commit, v1 of Lawn Tennis (Pong clone) complete.

---
+++ Assets/Scripts/GoalController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalController : MonoBehaviour {
+
+	public int player;
+	public AudioClip applause;
+
+	private AudioSource applauseSFX;
+
+	void Start () {
+		applauseSFX = GetComponent<AudioSource> ();
+	}
+	
+	void OnTriggerEnter2D (Collider2D other) {
+		GameObject gameObject = GameObject.FindGameObjectWithTag ("GameController");
+		ScoreController scoreController = gameObject.GetComponent<ScoreController> ();
+		scoreController.IncreaseScore (player);
+		applauseSFX.PlayOneShot (applause);
+	}
+}
---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour {
+
+	public Text scoreText;
+
+	private BallController ball;
+	private MainLightController mainLight;
+	private TextController scoreTextController;
+	private int score1;
+	private int score2;
+	private int player;
+
+	void Start () {
+		ball = GameObject.FindGameObjectWithTag ("ball").GetComponent<BallController>();
+		mainLight = GameObject.FindGameObjectWithTag ("mainLight").GetComponent<MainLightController> ();
+		scoreTextController = GameObject.FindGameObjectWithTag ("scoreText").GetComponent<TextController> ();
+	}
+
+	public void IncreaseScore(int player) {
+		if (player == 1) {
+			score1 += 1;
+		} else if (player == 2) {
+			score2 += 1;
+		}
+		scoreText.text = score1 + " - " + score2;
+
+		ball.ResetLaunch ();
+		mainLight.DimLights ();
+		scoreTextController.RaiseAlpha ();
+	}
+
+	/*	
+	 * GameObject gameObject = GameObject.FindGameObjectWithTag ("scoreText");
+	 * TextController scoreText = gameObject.GetComponent<TextController> ();
+	 * scoreText.IncreaseScore (player);
+	*/
+}
